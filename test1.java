62702fd24d84e93d38f36f09e2b8dc319b1a031a
Raju7815 <157611478+Raju7815@users.noreply.github.com>
2024-08-17 14:43:27 +0530

    Add files via upload
    this is a program about school management system

---
+++ test1.java
@@ -0,0 +1,115 @@
+    import java.util.*;
+
+public class test1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Teacher t = new Teacher();
+        Student st = new Student();
+
+        System.out.println("Enter the number of teacher details you want to enter:");
+        int x = sc.nextInt();
+        String[] name = new String[x];
+        int[] age = new int[x];
+        String[] subject = new String[x];
+        int[] salary = new int[x];
+
+        System.out.println("Enter the number of student details you want to enter:");
+        int y = sc.nextInt();
+        String[] names = new String[y];
+        int[] classes = new int[y];
+        String[] section = new String[y];
+
+        for (int i = 0; i < x; i++) {
+            System.out.println("Enter name of teacher " + (i + 1) + ":");
+            name[i] = sc.next();
+            System.out.println("Enter age of teacher " + (i + 1) + ":");
+            age[i] = sc.nextInt();
+            System.out.println("Enter subject of teacher " + (i + 1) + ":");
+            subject[i] = sc.next();
+            System.out.println("Enter salary of teacher " + (i + 1) + ":");
+            salary[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < x; i++) {
+            t.teacherdetails(name[i], age[i], subject[i], salary[i], i, x);
+        }
+
+        for (int i = 0; i < y; i++) {
+            System.out.println("Enter name of student " + (i + 1) + ":");
+            names[i] = sc.next();
+            System.out.println("Enter class of student " + (i + 1) + ":");
+            classes[i] = sc.nextInt();
+            System.out.println("Enter section of student " + (i + 1) + ":");
+            section[i] = sc.next();
+        }
+
+        for (int i = 0; i < y; i++) {
+            st.studentdetails(names[i], classes[i], section[i], i, y);
+        }
+
+        t.display();
+        st.display();
+    }
+}
+
+class Teacher {
+    String[] name;
+    int[] age;
+    String[] subject;
+    int[] salary;
+    int l;
+
+    Teacher() {
+        name = new String[100];
+        age = new int[100];
+        subject = new String[100];
+        salary = new int[100];
+    }
+
+    void teacherdetails(String name1, int age1, String subject1, int salary1, int x, int y) {
+        this.l = y;
+        this.name[x] = name1;
+        this.age[x] = age1;
+        this.subject[x] = subject1;
+        this.salary[x] = salary1;
+    }
+
+    void display() {
+        System.out.println("Teachers' details are:");
+        for (int i = 0; i < l; i++) {
+            System.out.println("Name: " + name[i]);
+            System.out.println("Age: " + age[i]);
+            System.out.println("Subject: " + subject[i]);
+            System.out.println("Salary: " + salary[i]);
+        }
+    }
+}
+
+class Student {
+    String[] name;
+    int[] Class;
+    String[] section;
+    int l;
+
+    Student() {
+        name = new String[100];
+        Class = new int[100];
+        section = new String[100];
+    }
+
+    void studentdetails(String name1, int Class1, String sec, int x, int y) {
+        this.name[x] = name1;
+        this.Class[x] = Class1;
+        this.section[x] = sec;
+        this.l = y;
+    }
+
+    void display() {
+        System.out.println("Students' details are:");
+        for (int i = 0; i < l; i++) {
+            System.out.println("Name: " + name[i]);
+            System.out.println("Class: " + Class[i]);
+            System.out.println("Section: " + section[i]);
+        }
+    }
+}
